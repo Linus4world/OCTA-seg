4afb931dd6ab7eec263b8b573f92a728a988dc28
Linus4world <linus.kreitner@tum.de>
2022-08-11 22:54:06 +0200

    refactor: use external metrics manager

---
+++ metrics.py
@@ -0,0 +1,112 @@
+
+from enum import Enum
+
+import numpy as np
+import torch
+from monai.metrics import DiceMetric, MeanIoU, ROCAUCMetric, CumulativeIterationMetric
+
+
+class Task(Enum):
+    VESSEL_SEGMENTATION = "ves-seg"
+    AREA_SEGMENTATION = "area-seg"
+    IMAGE_QUALITY_CLASSIFICATION = "img-qual-clf"
+    RETINOPATHY_CLASSIFICATION = "ret-clf"
+
+def confusion_matrix(rater_a, rater_b, min_rating=None, max_rating=None):
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(rater_a + rater_b)
+    if max_rating is None:
+        max_rating = max(rater_a + rater_b)
+    num_ratings = int(max_rating - min_rating + 1)
+    conf_mat = [[0 for i in range(num_ratings)]
+                for j in range(num_ratings)]
+    for a, b in zip(rater_a, rater_b):
+        conf_mat[a - min_rating][b - min_rating] += 1
+    return conf_mat
+
+def histogram(ratings, min_rating=None, max_rating=None):
+    if min_rating is None:
+        min_rating = min(ratings)
+    if max_rating is None:
+        max_rating = max(ratings)
+    num_ratings = int(max_rating - min_rating + 1)
+    hist_ratings = [0 for x in range(num_ratings)]
+    for r in ratings:
+        hist_ratings[r - min_rating] += 1
+    return hist_ratings
+
+
+def quadratic_weighted_kappa(rater_a, rater_b, min_rating=None, max_rating=None):
+    rater_a = np.array(rater_a, dtype=int)
+    rater_b = np.array(rater_b, dtype=int)
+    assert(len(rater_a) == len(rater_b))
+    if min_rating is None:
+        min_rating = min(min(rater_a), min(rater_b))
+    if max_rating is None:
+        max_rating = max(max(rater_a), max(rater_b))
+    conf_mat = confusion_matrix(rater_a, rater_b, min_rating, max_rating)
+    num_ratings = len(conf_mat)
+    num_scored_items = float(len(rater_a))
+
+    hist_rater_a = histogram(rater_a, min_rating, max_rating)
+    hist_rater_b = histogram(rater_b, min_rating, max_rating)
+
+    numerator = 0.0
+    denominator = 0.0
+
+    for i in range(num_ratings):
+        for j in range(num_ratings):
+            expected_count = (hist_rater_a[i] * hist_rater_b[j] / num_scored_items)
+            d = pow(i - j, 2.0) / pow(num_ratings - 1, 2.0)
+            numerator += d * conf_mat[i][j] / num_scored_items
+            denominator += d * expected_count / num_scored_items
+    return 1.0 - numerator / denominator
+
+class QuadraticWeightedKappa:
+    """
+    Implementation following https://github.com/zhuanjiao2222/DRAC2022/blob/main/evaluation/metric_classification.py
+    """
+    def __init__(self) -> None:
+        self.items = []
+
+    def __call__(self, y_pred: torch.Tensor, y: torch.Tensor) -> None:
+        self.items.append(quadratic_weighted_kappa(y,y_pred))
+
+    def aggregate(self) -> torch.Tensor:
+        if len(self.items) > 0:
+            return torch.Tensor(sum(self.items)/len((self.items)))
+        else:
+            return torch.zeros(1)
+
+    def reset(self):
+        self.items = []
+
+class MetricsManager():
+    def __init__(self, task: Task):
+        if task == Task.VESSEL_SEGMENTATION.value or task == Task.AREA_SEGMENTATION.value:
+            self.metrics = {
+                "DSC": DiceMetric(include_background=True, reduction="mean"),
+                "IoU": MeanIoU(include_background=True, reduction="mean")
+            }
+            self.comp = "DSC"
+        elif task == Task.IMAGE_QUALITY_CLASSIFICATION.value or task == Task.RETINOPATHY_CLASSIFICATION.value:
+            self.metrics =  {
+                "QwK": QuadraticWeightedKappa(),
+                "ROC_AUC": ROCAUCMetric(average="macro")
+            }
+            self.comp = "QwK"
+
+    def __call__(self, y_pred: torch.Tensor, y: torch.Tensor):
+        for v in self.metrics.values():
+            v(y_pred=y_pred, y=y)
+
+    def aggregate_and_reset(self, prefix: str = ''):
+        d = dict()
+        for k,v in self.metrics.items():
+            d[f'{prefix}_{k}'] = v.aggregate().item()
+            v.reset()
+        return d
+
+    def get_comp_metric(self, prefix: str):
+        return f'{prefix}_{self.comp}'
--- train.py
+++ train.py
@@ -8,7 +8,6 @@ import datetime
 from monai.data import decollate_batch
 from monai.utils import set_determinism
 from monai.metrics import DiceMetric, MeanIoU
-from monai.inferers import sliding_window_inference
 
 from monai.networks.nets import DynUNet
 import time
@@ -16,6 +15,7 @@ from tqdm import tqdm
 from cl_dice_loss import clDiceLoss
 
 from image_dataset import get_dataset, get_post_transformation
+from metrics import MetricsManager, Task
 from visualizer import Visualizer
 
 # Parse input arguments
@@ -28,18 +28,19 @@ path = os.path.abspath(args.config_file)
 with open(path) as filepath:
     config = json.load(filepath)
 
+max_epochs = config["Train"]["epochs"]
+val_interval = config["Train"]["val_interval"]
+VAL_AMP = config["General"]["amp"]
+dtype = torch.float16 if VAL_AMP else torch.float32
+device = torch.device(config["General"]["device"])
+task: Task = config["General"]["task"]
 set_determinism(seed=0)
+visualizer = Visualizer(config, args.config_file)
 
 train_loader = get_dataset(config, 'train')
 val_loader = get_dataset(config, 'validation')
 test_loader = get_dataset(config, 'test')
-
-max_epochs = config["Train"]["epochs"]
-val_interval = config["Train"]["val_interval"]
-VAL_AMP = config["General"]["amp"]
-
-device = torch.device(config["General"]["device"])
-
+post_trans = get_post_transformation()
 
 # Model
 num_layers = config["General"]["num_layers"]
@@ -52,44 +53,18 @@ model = DynUNet(
     strides=(1,*[2]*num_layers,1),
     upsample_kernel_size=(1,*[2]*num_layers,1),
 ).to(device)
-
-visualizer = Visualizer(config, args.config_file)
 visualizer.save_model_architecture(model, next(iter(train_loader))["image"].to(device=device))
 
-# loss_function = DiceLoss(smooth_nr=0, smooth_dr=1e-5, squared_pred=True, to_onehot_y=False, sigmoid=True)
 loss_function = clDiceLoss(alpha=config["Train"]["lambda_cl_dice"], sigmoid=True)
 optimizer = torch.optim.Adam(model.parameters(), 1e-4, weight_decay=1e-5)
 lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=max_epochs)
-
-dice_metric = DiceMetric(include_background=True, reduction="mean")
-mIoU_metric = MeanIoU(include_background=True, reduction="mean")
-
-post_trans = get_post_transformation()
-
+metrics = MetricsManager(task)
 # use amp to accelerate training
-scaler = torch.cuda.amp.GradScaler()
+scaler = torch.cuda.amp.GradScaler(enabled=VAL_AMP)
 
-# define inference method
-def inference(input):
-
-    def _compute(input):
-        return sliding_window_inference(
-            roi_size=(256,256),
-            inputs=input,
-            sw_batch_size=1,
-            predictor=model,
-            overlap=0.5
-        )
-
-    if VAL_AMP:
-        with torch.cuda.amp.autocast():
-            return _compute(input)
-    else:
-        return _compute(input)
 
 
 # TRAINING BEGINS HERE
-
 best_metric = -1
 best_metric_epoch = -1
 
@@ -104,16 +79,15 @@ for epoch in epoch_tqdm:
     for batch_data in mini_batch_tqdm:
         step += 1
         inputs, labels = (
-            batch_data["image"].to(device).half(),
-            batch_data["label"].to(device).half(),
+            batch_data["image"].to(device, dtype=dtype),
+            batch_data["label"].to(device, dtype=dtype),
         )
         optimizer.zero_grad()
         with torch.cuda.amp.autocast():
             outputs = model(inputs)
             loss = loss_function(outputs, labels)
             outputs = [post_trans(i) for i in decollate_batch(outputs)]
-            dice_metric(y_pred=outputs, y=labels)
-            mIoU_metric(y_pred=outputs, y=labels)
+            metrics(y_pred=outputs, y=labels)
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
@@ -122,17 +96,10 @@ for epoch in epoch_tqdm:
     lr_scheduler.step()
 
     epoch_loss /= step
-    metric_train = dice_metric.aggregate().item()
-    mIoU_metric_train = mIoU_metric.aggregate().item()
     epoch_metrics["loss"] = {
         "train_loss": epoch_loss
     }
-    epoch_metrics["metric"] = {
-        "train_dice": metric_train,
-        "train_mIoU": mIoU_metric_train
-    }
-    dice_metric.reset()
-    mIoU_metric.reset()
+    epoch_metrics["metric"] = metrics.aggregate_and_reset(prefix="train")
     epoch_tqdm.set_description(f'avg train loss: {epoch_loss:.4f}')
 
     if (epoch + 1) % val_interval == 0:
@@ -146,28 +113,24 @@ for epoch in epoch_tqdm:
                     val_data["image"].to(device).half(),
                     val_data["label"].to(device).half(),
                 )
-                val_outputs = inference(val_inputs)
                 with torch.cuda.amp.autocast():
+                    val_outputs = model(val_inputs)
                     val_loss += loss_function(val_outputs, val_labels).item()
                 val_outputs = [post_trans(i) for i in decollate_batch(val_outputs)]
-                dice_metric(y_pred=val_outputs, y=val_labels)
-                mIoU_metric(y_pred=val_outputs, y=val_labels)
+                metrics(y_pred=val_outputs, y=val_labels)
 
-            metric_val = dice_metric.aggregate().item()
-            mIoU_metric_val= mIoU_metric.aggregate().item()
             epoch_metrics["loss"]["val_loss"] = val_loss/step
-            epoch_metrics["metric"]["val_dice"] = metric_val
-            epoch_metrics["metric"]["val_mIoU"] = mIoU_metric_val
-            dice_metric.reset()
-            mIoU_metric.reset()
+            epoch_metrics["metric"].update(metrics.aggregate_and_reset(prefix="val"))
 
-            if metric_val > best_metric:
-                best_metric = metric_val
+            metric_comp =  epoch_metrics["metric"][metrics.get_comp_metric('val')]
+
+            if metric_comp > best_metric:
+                best_metric = metric_comp
                 best_metric_epoch = epoch + 1
                 visualizer.save_model(model)
 
             visualizer.plot_losses_and_metrics(epoch_metrics, epoch)
-            visualizer.plot_sample(val_inputs[0], val_outputs[0], val_labels[0], number= None if best_metric>metric_val else 'best')
+            visualizer.plot_sample(val_inputs[0], val_outputs[0], val_labels[0], number= None if best_metric>metric_comp else 'best')
     visualizer.log_model_params(model, epoch)
 
 total_time = time.time() - total_start
